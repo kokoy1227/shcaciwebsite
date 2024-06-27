8e933a60eaf61bca0e752bd88ebcda797b263421
johnrobertnadala <133988114+johnrobertnadala@users.noreply.github.com>
2024-06-27 17:55:59 +0800

    u

--- index.php
+++ index.php
@@ -439,7 +439,7 @@
 
     <!-- Industries Covered -->
     <section class="industries-covered" style="background-image: url(assets/images/background/bg-3.jpg);">
-        <div class="background-text" data-parallax='{"x": 100}'>Shepherd's Haven Christian Academy of Cavite (SHCACI), Inc.</div>
+        <div class="background-text" data-parallax='{"x": 100}'>SHCACI</div>
         <div class="outer-box side-container">
             <div class="outer-container">
                 <div class="theme_carousel owl-theme owl-carousel" data-options='{"loop": true, "center": true, "margin": 0, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 6000, "smartSpeed": 1000, "responsive":{ "0" :{ "items": "1" }, "600" :{ "items" : "1" }, "768" :{ "items" : "1" } , "992":{ "items" : "1" }, "1200":{ "items" : "1" }}}'>
@@ -459,7 +459,7 @@
                     </div>
                     <div class="text-block">
                         <div class="inner-box">
-                            <div class="image" style="background-image: url(assets/images/resource/image-4.jpg);">
+                            <div class="image" style="background-image: url(assets/images/resource/DSC_5715.jpg);">
                             </div>
                             <div class="content">
                                 <div class="icon"><span class="flaticon-spaceship"></span></div>
