b7cfc2c6d5f8a02f67f2c7a45f3848a92ae7c28c
johnrobertnadala <133988114+johnrobertnadala@users.noreply.github.com>
2024-06-28 09:44:35 +0800

    w

--- index.php
+++ index.php
@@ -500,7 +500,7 @@
                     </div>
                     <div class="text-block">
                         <div class="inner-box">
-                            <div class="image" style="background-image: url(assets/images/resource/DCS11351.JPG);">
+                            <div class="image" style="background-image: url(assets/images/resource/DCS11351.jpg);">
                             </div> 
                             <div class="content">
                                 <div class="icon"><span class="flaticon-box-1"></span></div>
