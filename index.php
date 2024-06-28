cf9b3e4b28528b40bffa0b49bbbab5c6b0a5ec01
Mark Daniel Barranco <133103325+kokoy1227@users.noreply.github.com>
2024-06-28 11:23:16 +0800

    image icons

--- index.php
+++ index.php
@@ -614,10 +614,10 @@
         <div class="auto-container">
             <div class="wrapper-box" style="background-image: url(assets/images/background/bg-4.jpg);">
                 <div class="shape">
-                    <div class="shape-one"><img src="assets/images/resource/icon1.png" alt=""></div>
-                    <div class="shape-two"><img src="assets/images/resource/icon2.png" alt=""></div>
-                    <div class="shape-three"><img src="assets/images/resource/icon3.png" alt=""></div>
-                    <div class="shape-four"><img src="assets/images/resource/icon4.png" alt=""></div>
+                    <div class="shape-one"><img src="assets/images/icons/icon1.png" alt=""></div>
+                    <div class="shape-two"><img src="assets/images/icons/icon2.png" alt=""></div>
+                    <div class="shape-three"><img src="assets/images/icons/icon3.png" alt=""></div>
+                    <div class="shape-four"><img src="assets/images/icons/icon4.png" alt=""></div>
                     <div class="shape-five"><img src="assets/images/resource/image-8.png" alt=""></div>
                 </div>
                 <div class="sec-title text-center light">
