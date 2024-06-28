bb4a13469bf147d4fcb83e8d4ae67dc3b2f0cf8d
johnrobertnadala <133988114+johnrobertnadala@users.noreply.github.com>
2024-06-28 11:49:47 +0800

    W

--- index.php
+++ index.php
@@ -609,6 +609,71 @@
         </div>
     </section> 
 
+        <!-- Pricing Section -->
+        <section class="pricing-section">
+        <div class="auto-container">
+            <div class="sec-title text-center">
+                <div class="sub-title text-center">School Fees</div>
+                <h4>Please be advised of the Schedule of Fees for S.Y. 2024-2025.</h4>
+                <h6>To know more about our payment options, kindly visit our Accounting Office, or call (046) 238-50.</h6>
+            </div>
+            <div class="row m-0">
+                <div class="col-lg-4 col-md-6 pricing-block">
+                    <div class="inner-box wow fadeInUp" data-wow-duration="1500ms">
+                        <div class="category-wrapper"><div class="category">Basic Plan</div></div>
+                        <div class="price">$89.99</div>
+                        <div class="time">Per Month</div>
+                        <ul class="content">
+                            <li>1 Warehouse </li>
+                            <li>Custom Business Rules</li>
+                            <li>Real Time Rate Shopping</li>
+                            <li>100% Insurance</li>
+                            <li>50 Freight Shipments</li>
+                        </ul>
+                        <div class="link-box">
+                            <a href="#" class="theme-btn btn-style-one"><span><i class="flaticon-up-arrow"></i>Buy Now </span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 pricing-block style-two">
+                    <div class="inner-box wow fadeInUp" data-wow-duration="1500ms" style="background-image: url(assets/images/resource/image-3.jpg);">
+                        <div class="category-wrapper"><div class="category">Standard Plan</div></div>
+                        <div class="price">$129.99</div>
+                        <div class="time">Per Month</div>
+                        <ul class="content">
+                            <li>1 Warehouse </li>
+                            <li>Custom Business Rules</li>
+                            <li>Real Time Rate Shopping</li>
+                            <li>100% Insurance</li>
+                            <li>50 Freight Shipments</li>
+                        </ul>
+                        <div class="link-box">
+                            <a href="#" class="theme-btn btn-style-one"><span><i class="flaticon-up-arrow"></i>Buy Now </span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 pricing-block">
+                    <div class="inner-box wow fadeInUp" data-wow-duration="1500ms">
+                        <div class="category-wrapper"><div class="category">Advanced Plan</div></div>
+                        <div class="price">$149.99</div>
+                        <div class="time">Per Month</div>
+                        <ul class="content">
+                            <li>1 Warehouse </li>
+                            <li>Custom Business Rules</li>
+                            <li>Real Time Rate Shopping</li>
+                            <li>100% Insurance</li>
+                            <li>50 Freight Shipments</li>
+                        </ul>
+                        <div class="link-box">
+                            <a href="#" class="theme-btn btn-style-one"><span><i class="flaticon-up-arrow"></i>Buy Now </span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+
+
     <!-- Facts Section -->
     <section class="facts-section">
         <div class="auto-container">
