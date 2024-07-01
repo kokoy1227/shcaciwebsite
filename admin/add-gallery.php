5b2409439ee5c7d90aed1ba1fe274a75c1bd8080
Mark Daniel Barranco <133103325+kokoy1227@users.noreply.github.com>
2024-07-01 11:44:51 +0800

    dad

--- admin/add-gallery.php
+++ admin/add-gallery.php
@@ -37,8 +37,8 @@ date_default_timezone_set('Asia/Kolkata');
 $today = date("D d M Y");
 $edit = $_GET['edit'];
 
- $resultt = mysqli_query($con,"SELECT * FROM teams where id=".$edit."");
- $roww = mysqli_fetch_array($resultt);
+$resultt = mysqli_query($con,"SELECT * FROM services where id=".$edit."");
+$roww = mysqli_fetch_array($resultt);
 
 if(isset($_POST['publise'])){
 	
