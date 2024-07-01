6f17dfaf7ae9dce1fe5c9823b5817f2934769adc
Mark Daniel Barranco <133103325+kokoy1227@users.noreply.github.com>
2024-07-01 11:47:19 +0800

    asda

--- admin/add-services.php
+++ admin/add-services.php
@@ -40,7 +40,7 @@ $today = date("D d M Y");
 $edit = $_GET['edit'];
 
  $resultt = mysqli_query($con,"SELECT * FROM services where id=".$edit."");
- $roww = mysqli_fetch_array($resultt);
+ $roww = mysqli_fetch_array($resultt);d
 
 if(isset($_POST['publise'])){
 	
