0c25d68ed92690c1ddfd0a7beab920488bb19a2b
Mark Daniel Barranco <133103325+kokoy1227@users.noreply.github.com>
2024-07-01 17:25:31 +0800

    admin logo

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -8,7 +8,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="assets/images/shcaci-logo.png" class="img-circle elevation-2" alt="User Image">
+          <img src="dist/img/admin-logo.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Admin Panel</a>
