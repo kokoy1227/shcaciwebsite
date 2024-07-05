3bc352c5cfd4fdfc420c130379a1e192135c8643
Mark Daniel Barranco <133103325+kokoy1227@users.noreply.github.com>
2024-07-05 18:16:39 +0800

    remove topbar

--- admin/topbar.php
+++ admin/topbar.php
@@ -6,7 +6,6 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a class="nav-link">Welcome to Admin Panel!!</a>
       </li>
       
     </ul>
